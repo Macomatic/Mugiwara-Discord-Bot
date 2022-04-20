d9f6997da0f549b14abf3293a8ca9d3577f92b80
Marco Vethanayagam <29697062+Macomatic@users.noreply.github.com>
2022-04-20 13:30:54 -0400

    findMALUser functionality working; API call working

---
+++ commands/findMALUser.js
@@ -0,0 +1,37 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
+const jikanjs = require('@mateoaranda/jikanjs');
+
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('findmaluser')
+        .setDescription('Gives the MAL account for the provided username')
+        .addStringOption(option => 
+            option.setName('username')
+                .setDescription('MAL username')
+                .setRequired(true)),
+    async execute(interaction) {
+        let username = (interaction.options.getString('username')).toLowerCase();
+        username = username.charAt(0).toUpperCase() + username.slice(1);
+
+        // line causing error
+        try {
+            const { data } = await jikanjs.loadUser(username);
+            const imageUrl = data.images.jpg.image_url;
+            const profileUrl = data.url;
+            const embed = new MessageEmbed()
+                .setTitle(`${username}'s MAL Account`)
+                .setDescription(profileUrl)
+                .setImage(imageUrl)
+                .addField('Anime List', `[Link](https://myanimelist.net/animelist/${username})`, true)
+                .addField('Manga List', `[Link](https://myanimelist.net/mangalist/${username})`, true)
+                .setFooter('/findmaluser command');
+            return interaction.reply({ embeds: [embed] });
+        }
+
+        catch (error) {
+            return interaction.reply('I could not find a MAL account with that username');
+        }
+    },
+};
