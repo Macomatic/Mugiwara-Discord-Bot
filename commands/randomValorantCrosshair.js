a7d69fd3e7c48cee6b4af1cf13003f667cf6a5da
Marco Vethanayagam <29697062+Macomatic@users.noreply.github.com>
2022-10-29 04:40:35 -0400

    randomValorantCrosshair functionality

--- commands/randomValorantCrosshair.js
+++ commands/randomValorantCrosshair.js
@@ -4,7 +4,7 @@ const { MessageEmbed } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('randvalcrosshair')
+        .setName('randomvalcrosshair')
         .setDescription('Generates a random valorant crosshair'),
     async execute(interaction) {
 
@@ -21,7 +21,7 @@ module.exports = {
             chcode += '0;s;1;P;';
         }
 
-        // c;[1,7] --> Determines color; no c;? param means white
+        // c;[1,7] --> Determines color; no c;? param means white, 1: Green, 2: Yellow Green, 3: Green Yellow, 4: Yellow, 5: Cyan, 6: Pink, 7: Red
         rand = Math.floor(Math.random() * 8);
         if (rand != 0) {
             chcode += `c;${rand};`;
@@ -70,6 +70,124 @@ module.exports = {
             chcode += 'm;1;';
         }
 
+        // Inner Lines Massive Chunk
+        // 0b;0 if inner lines off, otherwise alot of other options if on
+        rand = Math.floor(Math.random() * 2);
+        if (rand == 0) {
+            chcode += '0b;0;';
+        }
+        else {
+            // 0t;[0,10] inner line thickness 
+            rand = Math.floor(Math.random() * 11);
+            chcode += `0t;${rand};`;
+
+            // 0l;[0,20] inner line length
+            rand = Math.floor(Math.random() * 21);
+            chcode += `0l;${rand};`;
+
+            // 0o;[0,20] inner line offset
+            rand = Math.floor(Math.random() * 21);
+            chcode += `0o;${rand};`;
+
+            // 0a;[0.000,1.000] inner line opacity
+            rand = (Math.random() * (0.000 - 1.000) + 1.000).toFixed(3);
+            chcode += `0a;${rand};`;
+
+            // need to check firing error here because its placement depends on if its on or not
+            let firingError = 0;
+            firingError = Math.floor(Math.random() * 2);
+
+
+            // 0m;1 for movement error on, otherwise nothing for no
+            rand = Math.floor(Math.random() * 2);
+            if (rand == 1) {
+                chcode += '0m;1;';
+                
+                // one case where firing error is not on
+                if (firingError == 0) {
+                    chcode += '0f;0;';
+                }
+
+
+                // 0s;[0.000,3.000] for movement error multiplier
+                rand = (Math.random() * (0.000 - 3.000) + 3.000).toFixed(3);
+                chcode += `0s;${rand};`;
+
+                // one case where firing error is on, then we include the FE multi here; 0e;[0.000,3.000];
+                if (firingError == 1) {
+                    rand = (Math.random() * (0.000 - 3.000) + 3.000).toFixed(3);
+                    chcode += `0e;${rand};`;
+                }
+            }
+
+            else if (rand == 0) {
+                if (firingError == 0) {
+                    chcode += '0f;0;';
+                }
+                else {
+                    rand = (Math.random() * (0.000 - 3.000) + 3.000).toFixed(3);
+                    chcode += `0e;${rand};`;
+                }
+            }
+        }
+
+        // Outer Lines Massive Chunk
+        // 1b;0 if outer lines off, otherwise alot of other options if on
+        rand = Math.floor(Math.random() * 2);
+        if (rand == 0) {
+            chcode += '1b;0;';
+        }
+        else {
+            // 1t;[0,10] outer line thickness 
+            rand = Math.floor(Math.random() * 11);
+            chcode += `1t;${rand};`;
+
+            // 1l;[0,10] outer line length
+            rand = Math.floor(Math.random() * 11);
+            chcode += `1l;${rand};`;
+
+            // 1o;[0,40] outer line offset
+            rand = Math.floor(Math.random() * 41);
+            chcode += `1o;${rand};`;
+
+            // 1a;[0.000,1.000] outer line opacity
+            rand = (Math.random() * (0.000 - 1.000) + 1.000).toFixed(3);
+            chcode += `1a;${rand};`;
+
+            let movementError = 0;
+            let firingError = 0;
+            movementError = Math.floor(Math.random() * 2);
+            firingError = Math.floor(Math.random() * 2);
+
+            if (movementError == 0) {
+                chcode += '1m;0;';
+                if (firingError == 0) {
+                    chcode += '1f;0;';
+                }
+                else {
+                    rand = (Math.random() * (0.000 - 3.000) + 3.000).toFixed(3);
+                    chcode += `1e;${rand};`;
+                }
+            }
+            else if (movementError == 1) {
+                if (firingError == 0) {
+                    chcode += '1f;0;';
+                    rand = (Math.random() * (0.000 - 3.000) + 3.000).toFixed(3);
+                    chcode += `1s;${rand};`;
+                }
+                else {
+                    rand = (Math.random() * (0.000 - 3.000) + 3.000).toFixed(3);
+                    chcode += `1s;${rand};`;
+                    rand = (Math.random() * (0.000 - 3.000) + 3.000).toFixed(3);
+                    chcode += `1e;${rand};`;
+                }
+            }
+        }
+
+        // Ensuring removal of extra ; at the end of the ch code
+        if (chcode.lastIndexOf(';') + 1 == chcode.length) {
+            chcode = chcode.substring(0, chcode.length - 1);
+        }
 
 
         // ==================  RANDOMIZING CROSSHAIR END ==================
@@ -94,12 +212,12 @@ module.exports = {
 
         // creating embed for crosshair display in chat
         const embed = new MessageEmbed()
-            .setTitle('Valorant Crosshair')
+            .setTitle('Random Valorant Crosshair')
             .addField('Code', `${chcode}`, false)
-            .setColor('#47A4E4')
+            .setColor('#DA2BB2')
             .setImage(crosshair.url)
             .setFooter({
-                text: '/getvalorantch command',
+                text: '/randomvalcrosshair command',
             });
 
             return interaction.reply({ embeds: [embed] });
